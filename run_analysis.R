c204b8b716a7b498ef6507ce20024e7692621882
Samir Patel <samirpat@gmail.com>
2015-12-27 19:11:00 -0500

    Complete run_analysis.R script

--- run_analysis.R
+++ run_analysis.R
@@ -14,10 +14,10 @@ runAnalysis <- function()
   subject.trainObs <- read.table("UCI HAR Dataset/train/subject_train.txt")
   
   ##merge test data sets
-  testObs <- data.frame(subject.testObs,x.testObs,y.testObs)
+  testObs <- data.frame(subject.testObs,y.testObs,x.testObs)
   
   ##merge train data sets
-  trainObs <- data.frame(subject.trainObs,x.trainObs,y.trainObs)
+  trainObs <- data.frame(subject.trainObs,y.trainObs,x.trainObs)
   
   ##Combine Test and Train data sets
   mixObs <- rbind(testObs,trainObs)
@@ -31,15 +31,28 @@ runAnalysis <- function()
   ##set column names of merged data set
   colnames(mixObs) <- c("subjectId","activityLbl",columnNames)
   
+  ##removed duplicate column names from merge data sets
+  mixObs <- mixObs[ , !duplicated(colnames(mixObs))]
+  
+  ##select only subject , activity and columns with mean or std dev measurments
   mixObs <- select(mixObs,contains("subjectId"),
                           contains("activityLbl"),
-                          contains("-std"),
-                          contains("-mean"),
-                          -startswith("fBodyAcc-bandsEnergy"))
-  ##select only subject , activity and columns with mean or std dev measurments
- return (mixObs)
-              
+                          contains("std"),
+                          contains("mean"),
+                          -contains("freq"),
+                          -contains("angle"))
   
+  ##Read Activity Name and Activity Code
+  activityLbls <- read.table("UCI HAR Dataset/activity_labels.txt")
   
-
+  ##Replace Activity Code with Activity Name in data sets
+  mixObs$activityLbl <- as.character(activityLbls[match(mixObs$activityLbl,activityLbls$V1),'V2'])                    
+  
+  ##Group rows by subject and Activity and summarize mean of each measuremennts
+  data.summary <- mixObs %>% group_by(subjectId,activityLbl) 
+                        %>% summarise_each(funs(mean))
+  
+ 
+ ##write summary to a file summary.txt
+ write.table(data.summary,file="summary.txt",row.names = FALSE)
 }
